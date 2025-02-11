176382fc20d219b1f87963158abe679cf2675e05
Ubuntu <ubuntu@ip-172-31-44-129.ec2.internal>
2025-02-11 13:27:40 +0000

    test

--- src/app.py
+++ src/app.py
@@ -7,9 +7,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return "Esto es un test"
-
-@app.route('/ropa')
-def ropa():
-    return "Esto es una ruta ropa"
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=PORT)
